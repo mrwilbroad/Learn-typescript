05f3fd63d4463a813cff97df4168d49c7c7bff2c
mrwilbroad <wfrancis169@gmail.com>
2023-10-20 23:59:35 +0300

    func

--- app/Series-07/01morefunction.js
+++ app/Series-07/01morefunction.js
@@ -1,1 +1,9 @@
-// con
+// Function type Expression
+function greating(str, fn) {
+    return fn(str);
+}
+function Hi(s) {
+    console.log(s.toUpperCase());
+    return s.toUpperCase();
+}
+greating("Hello mrwilbroad", Hi);
--- app/Series-07/01morefunction.ts
+++ app/Series-07/01morefunction.ts
@@ -1,1 +1,13 @@
-// con
+// Function type Expression
+
+function greating(str: string ,fn: (s: string) => string) {
+  return fn(str);
+}
+
+
+function Hi(s: string){
+   console.log(s.toUpperCase());
+   return s.toUpperCase();
+}
+
+greating("Hello mrwilbroad",Hi);
